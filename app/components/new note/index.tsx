fc3be0ea22d10d9ff6b5c21245961b59d8794a80
Nicolle Interior <coeli@wizy.io>
2023-06-17 00:37:19 +0800

    Main + added show notes component

--- app/components/new note/index.tsx
+++ app/components/new note/index.tsx
@@ -1,19 +1,23 @@
-import { Container, Divider, Typography } from '@mui/material';
+import { Box, Container, Divider, Typography } from '@mui/material';
 
 import styles from './index.css'
 import NoteForm from './noteform';
+import ShowNotes from './shownotes';
 
 const NewNote = () => {
   return (
-    <Container className='wrapper'>
+    <Container sx={{marginTop: 15}}>
       <div>
         <Typography className='title' variant='h3'>
             My Notes
         </Typography>
         <Divider className='divider' />
       </div>
-      
-      <NoteForm />
+      <Box sx={{display: 'flex', flexDirection: 'row', padding: 2, marginTop: 2}} >
+        <NoteForm />
+        <ShowNotes isEmpty={true} />
+      </Box>
+     
     </Container>
   );
 };
--- app/components/new note/noteform.tsx
+++ app/components/new note/noteform.tsx
@@ -1,8 +1,34 @@
+import { Box, Button, TextField, Divider } from '@mui/material'
+import { Formik } from 'formik'
 import React from 'react'
 
 const NoteForm = () => {
   return (
-    <div style={{marginTop: 20}}>NoteForm</div>
+    <div style={{marginTop: 20, width: '100%'}}>
+        <Formik
+        initialValues={{
+            title: '',
+            content: ''
+        }}
+        onSubmit={() => {
+
+        }}
+        >
+            {
+                ({isSubmitting}) => (
+                    <Box sx={{display: 'flex', flexDirection: 'column'}} >
+                        <TextField label="Title" name="title" />
+                        <TextField label='Content' name='content' multiline minRows={1} maxRows={3} sx={{marginY: 4}} />
+                        <Button variant='contained' disabled={isSubmitting}>
+                            Submit
+                        </Button>
+                    </Box>
+                )
+            }
+
+        </Formik>
+        
+    </div>
   )
 }
 
